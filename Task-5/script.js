529960ce27a54e4d9f8468c16b8f47a0c4d75c93
Hina k <hina.kamal.dev@gmail.com>
2026-08-11 16:03:29 +0500

    Delete Task-5 directory

--- Task-5/script.js
+++
@@ -1,6 +0,0 @@
-let Navbar = document.querySelector(".navbar");
-let Menu = document.getElementById("nav_menu");
-
-Menu.addEventListener("click", () => {
-    Navbar.classList.toggle("active");
-});
